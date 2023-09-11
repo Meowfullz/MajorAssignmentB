4a33b3d149da7493714ede632d34771ee8fc6509
Gooby <129816575+Meowfullz@users.noreply.github.com>
2023-09-11 10:13:12 -0400

    Major Assignment Final

--- AssignmentB.py
+++ AssignmentB.py
@@ -8,16 +8,18 @@ vocabulary = {
     'table': {'French': 'table', 'German': 'tisch'},
     'book': {'French': 'livre', 'German': 'buchen'}}
 
-
+# Gets initial word to be translated
 word = input('Please enter an English word ')
 
-
+# if word entered exists in the vocabulary dictionary
 if word in vocabulary:
 	print('I will give you the French and German translations:', word)
 	translation = vocabulary[word] 
+	#prints the translations
 	print('The French translation is ', {translation['French']})
 	print('The German translation is ', {translation['German']})
 
+# if the word entered does not exist in the vocab dictionary, prints an error message
 else:
 	print('Error, This english word ', word, ' is not in my vocabulary!')
 	
